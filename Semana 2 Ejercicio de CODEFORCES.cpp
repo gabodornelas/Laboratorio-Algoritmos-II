3fea03233e619781b967ef9ef4d1949abe7a7bdc
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2024-05-22 09:34:05 -0400

    Update Semana 2 Ejercicio de CODEFORCES.cpp

--- Semana 2 Ejercicio de CODEFORCES.cpp
+++ Semana 2 Ejercicio de CODEFORCES.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 #include <vector>
-
+//A. Sasha and the Beautiful Array
+//Luego de recibir todos los valores, se ordenan de menor a mayor usando bubble sort y luego se hace una suma para determinar la "belleza" del arreglo.
 int main() 
 {
   int x,y,temp,suma;
@@ -10,7 +11,9 @@ int main()
   for(int k=0;k<x;k++){
     cin >> y;
     arreglo.resize(y);
-    for(int m=0;m<y;m++) cin >> arreglo[m];
+    for(int m=0;m<y;m++){
+      cin >> arreglo[m];
+    }
     for(int i=0; i<y; i++){
       for(int j=y-1; j>i; j--){
         if(arreglo[j] < arreglo[j-1]){
@@ -21,7 +24,9 @@ int main()
       }
     }
     suma=0;
-    for(int g=1;g<y;g++) suma=suma + arreglo[g] - arreglo[g-1];
+    for(int g=1;g<y;g++){
+      suma=suma + arreglo[g] - arreglo[g-1];
+    }
     cout << suma << endl;
   }
   return 0;
