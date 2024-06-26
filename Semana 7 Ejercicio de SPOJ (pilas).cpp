c9c826b742a99c8e7de724338655c20bed6c6cdc
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2024-06-26 14:38:09 -0400

    Update Semana 7 Ejercicio de SPOJ (pilas).cpp

--- Semana 7 Ejercicio de SPOJ (pilas).cpp
+++ Semana 7 Ejercicio de SPOJ (pilas).cpp
@@ -5,7 +5,7 @@ STACKEZ - Easy Stack
 Se implementaron las estructuras elemento (con valor (inicializado en 0) y apuntador al siguiente (inicializado en nulo)) y cola (con un
 primer elemento inicializado nulo, y los procedimientos, usando lista simplemente enlazada, de apilar (ubicando elementos al tope de la pila)
 y desapilar (quitando el elemento del tope de la pila y eliminandolo) ). Luego, dependiendo del caso, 1, 2 o 3, se apila, desapila o imprime
-el velor del primer elemento de la pila.
+el valor del primer elemento de la pila.
 /*/
 
 struct Elem{
